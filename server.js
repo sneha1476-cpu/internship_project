fc1c16848bfbeb338ca11ddcf262bfaa5a047b99
Sneha Patil <ssp142005@gmail.com>
2025-12-23 10:25:32 +0530

    added

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express=require('express')
-const app=express()
-const commonRouter=require('./routes/common_api')
+const express = require('express')
+const adminRouter = require('./routes/admin')
+const authUser = require('./utils/auth')
+const commonRouter = require('./routes/common_api')
+const videoRouter = require('./routes/video')   // ✅ ADD THIS
+
+const app = express()
+
 app.use(express.json())
+app.use(authUser)
 
-// URL:http://localhost:4000/auth/login
-app.use('/auth',commonRouter)
-// URL:http://localhost:4000/courses/
-app.use('/courses',commonRouter)
+// URL: http://localhost:4000/video/all-videos
+app.use('/video', videoRouter)
+// URL:http://localhost:4000/admin/enrolled-students
+app.use('/admin', adminRouter)
 
-app.listen(4000,'localhost',(req,res)=>{
-    console.log("Server is listening at port 4000");
-})
+app.use('/auth', commonRouter)
+app.use('/courses', commonRouter)
 
+app.listen(4000, 'localhost', () => {
+  console.log("Server is listening at port 4000")
+});
